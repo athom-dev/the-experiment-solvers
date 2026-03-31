dfa4eaae13ee061fa0c3bb46dd3ce8b18109b174
athom-dev <danielzavarizlara@gmail.com>
2026-03-31 03:56:33 -0300

    Backup load

---
+++ src/components/main/SolversTree.tsx
@@ -0,0 +1,99 @@
+import { Box } from "@chakra-ui/react"
+import { Link } from "react-router";
+import { TreeView, createTreeCollection } from "@chakra-ui/react"
+import { LuFile, LuFolder, LuExternalLink } from "react-icons/lu"
+import { useState } from "react"
+
+function Navigation(props: {showTitle?: boolean}) {
+    const [expandedValue, setExpandedValue] = useState<string[]>(["solvers", "more"]);
+    return (
+      <Box w={250}>
+          <TreeView.Root
+
+            collection={collection}
+            maxW="sm"
+            size="sm"
+            expandedValue={expandedValue}
+            onExpandedChange={(e) => setExpandedValue(e.expandedValue)}
+          >
+            {(props.showTitle == true) ?
+            (<TreeView.Label>Solvers and Helpers</TreeView.Label>): ''}
+            <TreeView.Tree >
+              <TreeView.Node
+                indentGuide={<TreeView.BranchIndentGuide />}
+                render={({ node, nodeState }) =>
+                  nodeState.isBranch ? (
+                    <TreeView.BranchControl>
+                      <LuFolder />
+                      <TreeView.BranchText>{node.name}</TreeView.BranchText>
+                    </TreeView.BranchControl>
+                  ) : (
+                    <TreeView.Item asChild>
+                        {node.disabled ? (
+                          <Box title={node.name + ": Currently unavailable"}>
+                            <LuFile />
+                            <TreeView.ItemText textWrap="nowrap" whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">{node.name}</TreeView.ItemText>
+                            {node.href?.startsWith("http") && (
+                              <LuExternalLink size={12} />
+                            )}
+                          </Box>
+                        ) : (
+                          <Link to={"/the-experiment-solvers/" + node.href} title={node.name}>
+                            <LuFile />
+                            <TreeView.ItemText textWrap="nowrap" whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">{node.name}</TreeView.ItemText>
+                            {node.href?.startsWith("http") && (
+                              <LuExternalLink size={12} />
+                            )}
+                          </Link>
+                        )}
+                    </TreeView.Item>
+                  )
+                }
+              />
+            </TreeView.Tree>
+          </TreeView.Root>
+      </Box>
+    );
+}
+
+interface Node {
+  id: string
+  href?: string
+  disabled?: boolean
+  name: string
+  children?: Node[]
+}
+
+const collection = createTreeCollection<Node>({
+  nodeToValue: (node) => node.id,
+  nodeToString: (node) => node.name,
+  rootNode: {
+    id: "ROOT",
+    name: "",
+    children: [
+      {
+        id: "solvers",
+        name: "Solvers",
+        children: [     
+          { disabled: true, href: "grid-puzzle.solv", id: "grid-puzzle.solv", name: "grid-puzzle.solv" },
+          { disabled: true, href: "math-puzzle.solv", id: "math-puzzle.solv", name: "math-puzzle.solv" },
+          { disabled: true, href: "chess-puzzle.solv", id: "chess-puzzle.solv", name: "chess-puzzle.solv" },
+          { href: "energy-container-puzzle.solv", id: "energy-container-puzzle.solv", name: "energy-container-puzzle.solv" },
+          { href: "xor-gates-puzzle.solv", id: "xor-gates-puzzle.solv", name: "xor-gates-puzzle.solv" },
+          { href: "batteries-puzzle.solv", id: "batteries-puzzle.solv", name: "batteries-puzzle.solv" },
+          { href: "unknown-signal-puzzle.solv", id: "unknown-signal-puzzle.solv", name: "unknown-signal-puzzle.solv" },
+        ],
+      },
+      {
+        id: "more",
+        name: "Helpers",
+        children: [
+          { href: 'lockdown.solv', id: "lockdown.solv", name: "lockdown.solv" },
+          { href: 'letter-to-number.solv', id: "letter-to-number.solv", name: "letter-to-number.solv" },
+        ],
+      },
+    ],
+  },
+})
+
+export default Navigation;
---
+++ src/pages/App.tsx
@@ -0,0 +1,87 @@
+import { Heading, Alert, Text, Box, Grid, List, Button, Card, Badge, Image, Stack, Separator } from "@chakra-ui/react"
+import { Link } from "react-router"
+
+const GreetingsText = () => (
+  <>
+      <Heading size="xl">The Experiment Solvers</Heading>
+      <Text fontSize="sm">This directory was developed with the goal of offering a high-quality and efficient website, centralizing all The Experiment Solvers in a single location. User experience (UX) was prioritized to ensure practicality and ease of interaction, taking into account the most efficient ways of use.</Text>
+      <Text fontSize="sm">Aware of the existence of Solvers already made available by other developers, this directory was conceived to centralize them in one place, facilitating access and providing intuitive navigation. </Text>
+      <Text fontSize="sm">Regarding the interface of each Solver, they were developed to be faithful to what is illustrated in The Experiment. However, in some cases, it was necessary to diverge from these standards, as this is a website that prioritizes user experience (UX).</Text>
+  </>
+)
+
+const Changelog = () => (
+  <>
+    
+      <Heading size="xl">18th February 2026 — Changelog <Badge>v4.0</Badge></Heading>
+      <Text fontSize="sm">These are the recent additions and changes to the directory:</Text>
+      <List.Root ps={6}>
+        <List.Item fontSize="sm">New solver <b>batteries-puzzle.solv</b> available;</List.Item>
+        <List.Item fontSize="sm">Added a <b>color mode</b> button to change website theme;</List.Item>
+        <List.Item fontSize="sm">Expanded Website's container area;</List.Item>
+        <List.Item fontSize="sm">Minor bug fixes and changes;</List.Item>
+      </List.Root>
+  </>
+)
+
+const SolverHelperItem = (props: {title: string, link: string, image: string, tag: string}) => (
+  <>
+    <Card.Root maxW="sm" gap={0} overflow="hidden">
+        <Image
+          aspectRatio={16 / 9}
+          objectFit="cover"
+          src={"/the-experiment-solvers/assets/"+props.image}
+        />
+        <Card.Header pt={4} px={4}>
+          <Card.Title textOverflow="ellipsis" whiteSpace="nowrap" width="100%" overflow="hidden"title={props.title}>{props.title}<br></br> <Badge variant="surface">{props.tag}</Badge></Card.Title>
+        </Card.Header>
+        <Card.Footer pt={0} p={4} justifyContent="flex-end">
+          <Link to={"/the-experiment-solvers/"+props.link}>
+              <Button size="xs" variant="solid">View solver</Button>
+          </Link>
+        </Card.Footer>
+      </Card.Root>
+  </>
+)
+
+const SolversAndHelpers = () => (
+  <>
+      <Stack>
+        <Heading size="xl">Available Solvers and Helpers</Heading>
+        <Grid className="solver-helper-list" gapY="6" gapX="6">
+          <SolverHelperItem title="lockdown.solv" link="lockdown.solv" image="lockdown.png" tag="Helpers"/>
+          <SolverHelperItem title="energy-container-puzzle.solv" link="energy-container-puzzle.solv" image="lab9.png" tag="Solvers"/>
+          <SolverHelperItem title="letter-to-number.solv" link="letter-to-number.solv" image="control-room.png" tag="Helpers"/>
+          <SolverHelperItem title="unknown-signal-puzzle.solv" link="unknown-signal-puzzle.solv" image="battery-e-room.png" tag="Solvers"/>
+          <SolverHelperItem title="xor-gates-puzzle.solv" link="xor-gates-puzzle.solv" image="lab9.png" tag="Solvers"/>
+          <SolverHelperItem title="batteries-puzzle.solv" link="batteries-puzzle.solv" image="lab9.png" tag="Solvers"/>
+        </Grid>
+      </Stack>
+  </>
+)
+
+function App() {
+  return (
+    <Box className="app-page" display="flex" flexDirection="column" gap={4}>
+      
+      <Alert.Root status="info">
+        <Alert.Indicator />
+        <Alert.Title>
+          Website under development — more solvers and helpers coming soon!
+        </Alert.Title>
+      </Alert.Root>
+
+
+
+      <GreetingsText/>
+      <Separator my={4} />
+
+      <SolversAndHelpers/>
+
+      <Separator my={4} />
+      <Changelog/>
+    </Box>
+  )
+}
+
+export default App
